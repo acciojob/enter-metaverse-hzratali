08328c5104870a8f715471d924ae2e563ebb3ffe
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-02 02:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const statusParagraph = document.getElementById('status');
+const enterButton = document.getElementById('enterBtn');
+
+enterButton.addEventListener('click', function() {
+	// Change the text to "Entered Metaverse" enclosed in an <h1> tag
+	statusParagraph.innerHTML = '<h1>Entered Metaverse</h1>';
+});
